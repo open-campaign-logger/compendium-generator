043777db30fc0c281a56da488d1ce5e1a6b27329
Cory Gill <talktocory@gmail.com>
2023-07-24 18:56:57 -0400

    Adding in support for external service configuration.

---
+++ src/CampaignKit.Compendium.Core/Configuration/Service.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="Service.cs" company="Jochen Linnemann - IT-Service">
+// Copyright (c) 2017-2023 Jochen Linnemann, Cory Gill.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace CampaignKit.Compendium.Core.Configuration
+{
+    /// <summary>
+    /// Represents a set of services for Tabletop Role-Playing Game (TTRPG) systems.
+    /// </summary>
+    public class Service
+    {
+
+        /// <summary>
+        /// Gets or sets the name of the service.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the endpoint of the service.
+        /// </summary>
+        public string Endpoint { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the key of the service.
+        /// </summary>
+        public string Key { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this service is active.
+        /// </summary>
+        public bool IsActive { get; set; } = true;
+    }
+}
